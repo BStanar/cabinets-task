aca4bf52ff01ea5472858ba548ce6cdd30ab66d4
Boris Stanar <bstanar96@gmail.com>
2026-03-29 16:36:15 +0200

    Models render above the plane.

--- src/components/Scene/models.tsx
+++ src/components/Scene/models.tsx
@@ -1,6 +1,10 @@
-"use client"
+"use client";
 import { useGLTF } from "@react-three/drei";
 import { ModelTransform } from "@/lib/firestoreService";
+import { ThreeEvent } from "@react-three/fiber";
+import { useMemo, useRef } from "react";
+import * as THREE from "three";
+import { clone } from "three/examples/jsm/utils/SkeletonUtils.js";
 
 interface ModelProps {
   path: string;
@@ -9,14 +13,44 @@ interface ModelProps {
 
 export default function Model({ path, transform }: ModelProps) {
   const { scene } = useGLTF(path);
+  const groupRef = useRef<THREE.Group>(null);
 
-  const cloned = scene.clone(true);
+  const cloned = useMemo(() => scene.clone(true), [scene]);
+  const yOffset = useMemo(() => {
+    const box= new THREE.Box3().setFromObject(cloned);
+    return -box.min.y
+  }, [cloned])
+
+  const handleClick = (e: ThreeEvent<MouseEvent>) => {
+    e.stopPropagation();
+    if (!groupRef.current) return;
+
+    const worldPos = new THREE.Vector3();
+    groupRef.current.getWorldPosition(worldPos);
+    console.log(`[${path}] position:`, {
+      x: worldPos.x,
+      y: worldPos.y,
+      z: worldPos.z,
+    });
+  };
 
   return (
-    <primitive
-      object={cloned}
-      position={[transform.position.x, transform.position.y, transform.position.z]}
-      rotation={[transform.rotation.x, transform.rotation.y, transform.rotation.z]}
-    />
+    
+    <group
+      ref={groupRef}
+      onClick={handleClick}
+      position={[
+        transform.position.x,
+        transform.position.y,
+        transform.position.z,
+      ]}
+      rotation={[
+        transform.rotation.x,
+        transform.rotation.y,
+        transform.rotation.z,
+      ]}
+    >
+      <primitive object={cloned}  position={[0, yOffset, 0]} />
+    </group>
   );
 }
--- src/components/orbit-controls.tsx
+++
@@ -1,6 +0,0 @@
-import { useOrbitControls } from "@/hooks/use-orbit-controls";
-
-export default function OrbitControls() {
-   useOrbitControls()
-   return null;
-}
